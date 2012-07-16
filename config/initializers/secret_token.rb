d5d5ef079a77ba669f1cbd60856be6ffd1dede4c
Jonathan Sumrall <jmsumrall@gmail.com>
2012-07-16 19:17:48 -0400

    first commit - Basic user models and sessions implemented

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd3880e3d5e9d62fcc04414e73624d2ab4b9b33e4d8a1a296a97264e1220296423c6541b11d443698a07484ef965b00df7e7ff988c24f749f090fee720d180f15'
